5c78f44bd05249baf6a3aeb9a8cef8fb6cc74002
Gustavo Emiliano <nube.gustavosantos@tokiomarine.com.br>
2025-08-11 13:44:17 -0300

    add: project startup

---
+++ backend/src/main/java/com/emiliano/tarefa_crud/TarefaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.emiliano.tarefa_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaCrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/emiliano/tarefa_crud/TarefaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emiliano.tarefa_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
